030dcf7b3308013837e706d863ec719dcceeb824
Phillippi Aguiar <areias_2@hotmail.com>
2021-01-21 14:13:06 -0300

    Initial commit

---
+++ Example/Favoritos/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Favoritos
+//
+//  Created by areias_2@hotmail.com on 01/21/2021.
+//  Copyright (c) 2021 areias_2@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
